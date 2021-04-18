8a1afab579f47eabe113860d9a20f0c174c3733b
pedkhod <pkhoddami@gmail.com>
2021-04-18 18:49:23 +0430

    final1

--- rotor.py
+++ rotor.py
@@ -1,2 +1,20 @@
 #
 #
+import pickle
+import random
+alphabet='abcdefghijklmnopqrstuvwxyz'
+r2=list(alphabet)
+r3=list(alphabet)
+r1=list(alphabet)
+
+#print (r1)
+random.shuffle(r1)
+print (r1)
+random.shuffle(r2)
+random.shuffle(r3)
+print(r1)
+print(r2)
+print(r3)
+f= open('./t_r_s', 'wb')
+pickle.dump((r1,r2,r3),f)
+f.close()
